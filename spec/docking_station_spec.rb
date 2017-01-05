c45a868591c252da3cca1c1d569321cf3f47df21
Rob Holden <robholdenflag@gmail.com>
2017-01-05 11:57:20 +0000

    commiting for collaboration

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,14 @@ require 'docking_station'
 
 describe DockingStation do 
 
- it { is_expected.to respond_to :release_bike }
+ it { should respond_to :release_bike }
 
+ it 'Gets a bike' do 
+ 	docking_station.release_bike should be_a(Bike)
+ end
+
+ it { should respond_to (working?) }
+ it release_bike is_bike
+ should respond_to(working?)
 end
 
