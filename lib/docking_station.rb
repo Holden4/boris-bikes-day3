91ec7e728bc31f93c4267b6e69598be936961a80
Rob Holden <robholdenflag@gmail.com>
2017-01-06 15:22:16 +0000

    Created array for bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,6 +4,10 @@ class DockingStation
 
 	attr_reader :bike
 
+	def inititalize
+		@bikes = []
+	end
+
 	def release_bike
 		fail 'No bikes available!' unless self.bike
 		self.bike
@@ -11,6 +15,8 @@ class DockingStation
 
 	def dock(bike)
 		fail 'Station is full!' if !bike
-		@bike = bike
+		@bikes << bike
+
 	end
+
 end
